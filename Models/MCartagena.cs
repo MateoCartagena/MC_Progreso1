7c656d7ff37b6e3ae931414a9e11a28aaea51354
Mateo Cartagena <mateo.cartagena@udla.edu.ec>
2023-04-24 07:53:23 -0500

    Se creo la clase Mcartagena en Model con atributos varios

---
+++ Models/MCartagena.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MC_Progreso1.Models
+{
+    public class MCartagena
+    {
+        public int MC_Edad { get; set; }
+        //[Range(5, 10)]
+        public decimal MC_PromedioGlobal { get; set; }
+        //[StringLength(10)]
+        //ErrorMessage = "Last Name should be minimum 3 characters and a maximum of 50 characters")]
+        public string? MC_SegundoNombre { get; set; }
+        public Boolean MC_Soltero { get; set; }
+        public DateTime MC_FechaHoy { get; set; }
+
+    }
+}
